91ffdc834a49fda733eb546582bf64953e39754e
Prachi <prachikale136@gmail.com>
2023-03-14 23:39:31 +0530

    [Prachi] Add: Enetr valid first name using regex

---
+++ UserRegestrationUsingRegex/UserRegestrationUsingRegex/Pattern.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegestrationUsingRegex
+{
+   
+    internal class Pattern
+    {
+        //Prachi
+        public static string FirstName = "^[A-Z][a-z]{2,}$";
+        public static string LastName = "^[A-Z][a-z]{2,}$";
+        public static bool ValidateString(string sample)
+        {
+            return Regex.IsMatch(sample, FirstName);
+        }
+
+        
+    }
+}
---
+++ UserRegestrationUsingRegex/UserRegestrationUsingRegex/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace UserRegestrationUsingRegex
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("");
+            string firstName = "Prachi";
+           
+            if (Pattern.ValidateString(firstName))
+            {
+                Console.WriteLine("valid");
+            }
+            else
+            {
+                Console.WriteLine("invalid");
+            }
+            Console.ReadKey();
+
+
+
+
+        }
+
+    }
+}
